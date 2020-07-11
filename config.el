1b59dcdcc2f5d2600b11a684e32d69431f0bfe6c
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2020-07-11 22:21:27 +0800

    Initial commit

---
+++ config.el
@@ -0,0 +1,98 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Hung-Yi Loo"
+      user-mail-address "hungyi.loo@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-nord)
+
+;; Set a custom font
+(setq doom-font (font-spec :family "Source Code Pro" :size 14))
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Notes/")
+
+(after! org
+  ;; Log CLOSED timestamp when notes are set to DONE state
+  (setq org-log-done 'time)
+  ;; Show more whitespace in org mode when cycling
+  (setq org-cycle-separator-lines -1)
+  ;; Always use ID properties to store links
+  (setq org-id-link-to-org-use-id 'use-existing))
+
+(after! evil
+  ;; Don't move backwards
+  (setq evil-move-cursor-back nil)
+  (setq evil-move-beyond-eol t))
+
+(after! centaur-tabs
+  (setq centaur-tabs-modified-marker "●")
+  (setq centaur-tabs-height 32)
+  (setq centaur-tabs-set-bar nil)
+  (centaur-tabs-change-fonts "Segoe UI" 100)
+  (setq centaur-tabs-style "wave"))
+
+;; When storing links by ID, add them to the normal `org-stored-links' variable
+(defadvice! +org--store-id-link-a (link)
+  :filter-return #'org-id-store-link
+  (when (and link org-store-link-plist)
+    (add-to-list 'org-stored-links
+                 (list (plist-get org-store-link-plist :link)
+                       (plist-get org-store-link-plist :description))))
+  link)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(tab-line ((t (:inherit variable-pitch :foreground "black" :height 0.9)))))
