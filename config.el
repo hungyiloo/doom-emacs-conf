94f09ac471072315e676a5257d81122bfe479bd8
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2020-07-16 20:20:04 +0800

    Make default window (frame) bigger

--- config.el
+++ config.el
@@ -31,6 +31,8 @@
 (setq doom-font (font-spec :family "Cascadia Code" :size 15)
       doom-variable-pitch-font (font-spec :family "Segoe UI"))
 
+(setq initial-frame-alist '((width . 141) (height . 55) (fullscreen . fullheight)))
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/Notes/")
