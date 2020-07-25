b8cf54dfcd8b4f690294c6d5ee6533e9ec7766c9
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2020-07-25 18:05:26 +0800

    Remove redundant/unused

--- config.el
+++ config.el
@@ -132,14 +132,6 @@
   ;; Enable richer icons on neotree
   (setq doom-themes-neotree-enable-file-icons t))
 
-;; When storing links by ID, add them to the normal `org-stored-links' variable
-(defadvice! +org--store-id-link-a (link)
-  :filter-return #'org-id-store-link
-  (when (and link org-store-link-plist)
-    (add-to-list 'org-stored-links
-                 (list (plist-get org-store-link-plist :link)
-                       (plist-get org-store-link-plist :description))))
-  link)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -180,44 +172,3 @@
  ;; If there is more than one, they won't work right.
  '(tab-line ((t (:inherit variable-pitch :foreground "black" :height 0.9)))))
 
-;; (use-package! composite
-;;   :defer t
-;;   :init
-;;   (defvar composition-ligature-table (make-char-table nil))
-;;   :hook
-;;   (((prog-mode conf-mode nxml-mode markdown-mode help-mode)
-;;     . (lambda () (setq-local composition-function-table composition-ligature-table))))
-;;   :config
-;;   ;; support ligatures, some toned down to prevent hang
-;;   (when (version<= "27.0" emacs-version)
-;;     (let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
-;;                    (35 . ".\\(?:###\\|##\\|_(\\|[#(?[_{]\\)")
-;;                    (36 . ".\\(?:>\\)")
-;;                    (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
-;;                    (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
-;;                    (42 . ".\\(?:\\(?:\\*\\*/\\)\\|\\(?:\\*[*/]\\)\\|[*/>]\\)")
-;;                    (43 . ".\\(?:\\(?:\\+\\+\\)\\|[+>]\\)")
-;;                    (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
-;;                    (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
-;;                    (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
-;;                    (48 . ".\\(?:x[a-zA-Z]\\)")
-;;                    (58 . ".\\(?:::\\|[:=]\\)")
-;;                    (59 . ".\\(?:;;\\|;\\)")
-;;                    (60 . ".\\(?:\\(?:!--\\)\\|\\(?:~~\\|->\\|\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[*$+~/<=>|-]\\)")
-;;                    (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
-;;                    (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
-;;                    (63 . ".\\(?:\\(\\?\\?\\)\\|[:=?]\\)")
-;;                    (91 . ".\\(?:]\\)")
-;;                    (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
-;;                    (94 . ".\\(?:=\\)")
-;;                    (119 . ".\\(?:ww\\)")
-;;                    (123 . ".\\(?:-\\)")
-;;                    (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
-;;                    (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)")
-;;                    )
-;;                  ))
-;;       (dolist (char-regexp alist)
-;;         (set-char-table-range composition-ligature-table (car char-regexp)
-;;                               `([,(cdr char-regexp) 0 font-shape-gstring]))))
-;;     (set-char-table-parent composition-ligature-table composition-function-table))
-;;   )
