53ef90a4bca61b6896233de0b3400c4a454ee19a
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2020-09-20 19:14:10 +0800

    Use light modeline and disable minimap for speed

--- init.el
+++ init.el
@@ -33,8 +33,8 @@
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
        ;;indent-guides     ; highlighted indent columns
-       minimap           ; show a map of the code on the side
-       modeline          ; snazzy, Atom-inspired modeline, plus API
+       ;; minimap           ; show a map of the code on the side
+       (modeline +light)          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
