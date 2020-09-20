adf1bd8ce1490ab64353a6a7b7e4973c62908720
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2020-09-20 19:07:28 +0800

    Hide line numbers by default when olivetti

--- config.el
+++ config.el
@@ -52,8 +52,13 @@
 (use-package! olivetti
   :hook (org-mode . olivetti-mode)
   :hook (markdown-mode . olivetti-mode)
+  :hook (olivetti-mode . my-olivetti-setup)
   :init
-  (setq olivetti-body-width 80))
+  (setq olivetti-body-width 80)
+  (defun my-olivetti-setup ()
+    (setq doom--line-number-style nil)
+    (setq display-line-numbers nil)))
+
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
