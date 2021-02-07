caf2de958e172d52fa53de02ea4cb550575bb36c
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-02-07 10:17:05 +0800

    Don't use hardcoded home dir paths

--- config/lsp-mode.el
+++ config/lsp-mode.el
@@ -4,15 +4,15 @@
   (setq lsp-enable-symbol-highlighting nil)
   (setq lsp-clients-angular-language-server-command
         '("node"
-          "/home/hungyi/.config/yarn/global/node_modules/@angular/language-server"
+          (doom-path (getenv "HOME") "/.config/yarn/global/node_modules/@angular/language-server")
           "--ngProbeLocations"
-          "/home/hungyi/.config/yarn/global/node_modules"
+          (doom-path (getenv "HOME") "/.config/yarn/global/node_modules")
           "--tsProbeLocations"
-          "/home/hungyi/.config/yarn/global/node_modules"
+          (doom-path (getenv "HOME") "/.config/yarn/global/node_modules")
           "--stdio"))
   (setq lsp-clients-typescript-server-args
-        '("--stdio"
-          "--tsserver-path=/home/hungyi/.yarn/bin/tsserver"))
+        `("--stdio"
+          ,(concat "--tsserver-path=" (getenv "HOME") "/.yarn/bin/tsserver")))
   ;; (setq lsp-headerline-breadcrumb-enable t)
   ;; (setq lsp-headerline-breadcrumb-enable-diagnostics nil)
   (setq lsp-semantic-tokens-enable t))
