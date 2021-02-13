c01158e0176489695819b8257e7cb1796f289fc4
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2021-02-13 16:40:13 +0800

    Enhance smartparens hydra

--- lisp/hydras.el
+++ lisp/hydras.el
@@ -53,52 +53,68 @@ Current pattern: %`evil-mc-pattern
   "
 ^Navigation^           ^Editing^
 ^^^^----------------------------------------------------------------------------
-_l_: next sexp         _X_: kill hybrid      _T_: transpose hybrid
-_h_: backward sexp     _x_: kill sexp        _t_: transpose
-_L_: forward sexp      _<_: barf backward    _r_: raise
-_H_: previous sexp     _>_: barf forward     _/_: split
-_k_: up sexp (back)    _,_: slurp backward   _|_: splice
-_j_: down sexp         _._: slurp forward    _=_: clone
-_K_: down sexp (back)  _?_: slurp hybrid     _<delete>_: splice kill forward
-_J_: up sexp           _d_: kill whole line  _<backspace>_: splice kill backward
-_b_: sexp beginning    _c_: comment
-_e_: sexp end
+_e_: forward sexp      _d_: kill sexp        _~_: transpose
+_b_: backward sexp     _D_: kill whole line  _!_: raise
+_w_: next sexp         _c_: comment          _+_: join
+_W_: up sexp (forward) _<_: barf backward    _|_: split
+_B_: sexp beginning    _>_: barf forward     _=_: clone
+_E_: sexp end          _,_: slurp backward   _%_: convolute
+_RET_: down sexp       _._: slurp forward    ___: splice
+_v_: select sexp       _)_: wrap round       _<delete>_: splice kill forward
+_V_: clear selection   _]_: wrap square      _<backspace>_: splice kill backward
+_y_: yank selection    _}_: wrap curly       _p_: paste after
+_Y_: yank sexp         ^ ^                   _P_: paste before
 
-_u_: undo  _C-r_: redo  _v_: set mark  _V_: set linewise mark
-
-_s_: toggle strict
+_u_: undo  _C-r_: redo  _C-SPC_: set mark _s_: toggle strict
 "
-  ("l" #'sp-next-sexp)
-  ("h" #'sp-backward-sexp)
-  ("L" #'sp-forward-sexp)
-  ("H" #'sp-previous-sexp)
-  ("b" #'sp-beginning-of-sexp)
-  ("e" #'sp-end-of-sexp)
-  ("k" #'sp-backward-up-sexp)
-  ("j" #'sp-down-sexp)
-  ("J" #'sp-up-sexp)
-  ("K" #'sp-backward-down-sexp)
-  ("v" #'evil-visual-char)
-  ("V" #'evil-visual-line)
-  ("X" #'sp-kill-hybrid-sexp)
-  ("x" #'sp-kill-sexp)
-  ("d" #'sp-kill-whole-line)
+  ("0" #'evil-beginning-of-line)
+  ("$" #'evil-end-of-line)
+  ("^" #'evil-first-non-blank)
+  ("j" #'evil-next-line)
+  ("k" #'evil-previous-line)
+  ("h" #'evil-backward-char)
+  ("l" #'evil-forward-char)
+  ("y" #'evil-yank)
+  ("Y" #'sp-copy-sexp)
+  ("p" #'evil-paste-after)
+  ("P" #'evil-paste-before)
+  ("e" #'sp-forward-sexp)
+  ("b" #'sp-backward-sexp)
+  ("w" #'sp-next-sexp)
+  ("B" #'sp-beginning-of-sexp)
+  ("E" #'sp-end-of-sexp)
+  ("W" #'sp-up-sexp)
+  ("RET" #'sp-down-sexp)
+  ("v" #'sp-mark-sexp)
+  ("V" #'evil-exit-visual-state)
+  ("o" #'exchange-point-and-mark)
+  ("d" #'sp-kill-sexp)
+  ("D" #'sp-kill-whole-line)
   ("c" #'evilnc-comment-operator)
+  ("(" #'sp-wrap-round)
+  (")" #'sp-wrap-round)
+  ("[" #'sp-wrap-square)
+  ("]" #'sp-wrap-square)
+  ("{" #'sp-wrap-curly)
+  ("}" #'sp-wrap-curly)
   ("<" #'sp-backward-barf-sexp)
   (">" #'sp-forward-barf-sexp)
   ("," #'sp-backward-slurp-sexp)
   ("." #'sp-forward-slurp-sexp)
-  ("?" #'sp-slurp-hybrid-sexp)
-  ("t" #'sp-transpose-sexp)
-  ("T" #'sp-transpose-hybrid-sexp)
-  ("r" #'sp-raise-sexp)
-  ("/" #'sp-split-sexp)
-  ("|" #'sp-splice-sexp)
+  ("~" #'sp-transpose-sexp)
+  ("!" #'sp-raise-sexp)
+  ("+" #'sp-join-sexp)
+  ("|" #'sp-split-sexp)
+  ("=" #'sp-clone-sexp)
+  ("%" #'sp-convolute-sexp)
+  ("_" #'sp-splice-sexp)
   ("<delete>" #'sp-splice-sexp-killing-forward)
   ("<backspace>" #'sp-splice-sexp-killing-backward)
-  ("=" #'sp-clone-sexp)
-  ("s" #'smartparens-strict-mode :exit t)
+  ("z" #'evil-scroll-line-to-center)
+  ("C-SPC" #'evil-visual-char)
   ("u" #'evil-undo)
   ("C-r" #'evil-redo)
+  ("s" #'smartparens-strict-mode :exit t)
   ("q" nil "quit" :color blue)
+  ("C-g" nil "quit" :color blue)
   ("<escape>" nil "quit" :color blue))
