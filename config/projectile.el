720de481dbb95f3197c50908dfab4f1daf71db54
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2021-02-14 15:34:26 +0800

    Add more featureful project-switch-commands

--- config/projectile.el
+++ config/projectile.el
@@ -1,4 +1,16 @@
 ;;; config/projectile.el -*- lexical-binding: t; -*-
 
+(after! project
+  (map! :map project-prefix-map
+        "m" #'magit-status
+        "g" #'consult-ripgrep
+        "v" #'projectile-run-vterm)
+  (setq project-switch-commands
+        '((project-find-file "Find file")
+          (consult-ripgrep "Ripgrep")
+          (project-dired "Dired")
+          (magit-status "Magit")
+          (projectile-run-vterm "VTerm"))))
+
 (after! projectile
   (setq projectile-kill-buffers-filter 'kill-all))
