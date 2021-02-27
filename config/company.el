ec886f030dd6291bc9dee2515359de90cacf6246
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2021-02-27 19:44:21 +0800

    Make ESC cancel company before evil-normal-state

--- config/company.el
+++ config/company.el
@@ -2,4 +2,14 @@
 
 (after! company
   ;; More responsive company autocompletion
-  (setq company-idle-delay 0.1))
+  (setq company-idle-delay 0.1)
+  (after! evil
+    ;; Preserve my muscle memory and allow the escape key to
+    ;; cancel the company popup without messing with evil state.
+    ;; If no company popup is shown, escape will go back to normal state.
+    (advice-add #'evil-normal-state
+                :around
+                (defun my/company-quit-before-evil-normal-state (orig-fun &rest args)
+                  (if company-candidates
+                      (company-cancel)
+                    (apply orig-fun args))))))
