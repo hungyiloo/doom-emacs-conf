b0405458af06c73418665b59beeb0a4613144765
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2021-03-01 21:01:02 +0800

    More company tweaks

--- config/company.el
+++ config/company.el
@@ -2,14 +2,19 @@
 
 (after! company
   ;; More responsive company autocompletion
-  (setq company-idle-delay 0.1)
-  (after! evil
-    ;; Preserve my muscle memory and allow the escape key to
-    ;; cancel the company popup without messing with evil state.
-    ;; If no company popup is shown, escape will go back to normal state.
-    (advice-add #'evil-normal-state
-                :around
-                (defun my/company-quit-before-evil-normal-state (orig-fun &rest args)
-                  (if company-candidates
-                      (company-cancel)
-                    (apply orig-fun args))))))
+  (setq company-idle-delay 0.15)
+
+  ;; REVIEW: This doesn't really feel good to me. Maybe I've gotten used to the
+  ;; C-g way to cancel company already
+  ;;
+  ;; (after! evil
+  ;;   ;; Preserve my muscle memory and allow the escape key to
+  ;;   ;; cancel the company popup without messing with evil state.
+  ;;   ;; If no company popup is shown, escape will go back to normal state.
+  ;;   (advice-add #'evil-normal-state
+  ;;               :around
+  ;;               (defun my/company-quit-before-evil-normal-state (orig-fun &rest args)
+  ;;                 (if company-candidates
+  ;;                     (company-cancel)
+  ;;                   (apply orig-fun args)))))
+  )
