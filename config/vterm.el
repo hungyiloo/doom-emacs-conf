24eaa22cdac980127d0430e938ccf9125fe31d68
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-03-06 13:54:10 +0800

    Allow the use of consult-yank (and co) in vterm

--- config/vterm.el
+++ config/vterm.el
@@ -2,4 +2,22 @@
 
 (after! vterm
   ;; Start vterm in normal mode always
-  (evil-set-initial-state 'vterm-mode 'normal))
+  (evil-set-initial-state 'vterm-mode 'normal)
+
+  (advice-add #'insert-for-yank
+              :around
+              (defun my/insert-for-yank-vterm-shim (orig-fun &rest args)
+                (if (eq major-mode 'vterm-mode)
+                    (let ((inhibit-read-only t))
+                      (apply #'vterm-insert args))
+                  (apply orig-fun args))))
+
+  (after! consult
+    (defun my/vterm-consult-yank-pop (&optional arg)
+      "A `consult-yank-pop' wrapper for vterm compatibility."
+      (interactive "p")
+      (let ((inhibit-read-only t))
+        (consult-yank-pop arg)))
+
+    (map! :map 'vterm-mode-map
+          :nvi "M-y" #'my/vterm-consult-yank-pop)))
