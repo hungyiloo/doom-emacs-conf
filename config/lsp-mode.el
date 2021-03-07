2123347ac6c3d69e7e421c8d75cd7b3d428f6c96
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2021-03-07 16:58:30 +0800

    Add a note about TSC_NONPOLLING_WATCHER, but not sure it works

--- config/lsp-mode.el
+++ config/lsp-mode.el
@@ -5,6 +5,10 @@
   (setq +lsp-defer-shutdown nil)
   (setq lsp-enable-symbol-highlighting nil)
   (setq lsp-enable-file-watchers nil)
+
+  ;; NOTE: Angular/TS language servers might perform better if we have this
+  ;; (setenv "TSC_NONPOLLING_WATCHER" "1")
+
   (setq lsp-clients-angular-language-server-command
         `("node"
           ,(doom-path (getenv "HOME") ".config/yarn/global/node_modules/@angular/language-server")
