fc7f8a0408d7588fee6f8c722c0328ebff961fce
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2021-03-10 17:33:02 +0800

    More consistent mappings for occur/wgrep

--- config/consult.el
+++ config/consult.el
@@ -13,4 +13,14 @@
          "I" #'consult-project-imenu
          "S" #'+compres/consult-line-symbol-at-point
          :desc "Search for file" "f" #'consult-find
-         :desc "Locate file" "F" #'consult-locate)))
+         :desc "Locate file" "F" #'consult-locate))
+
+  ;; Make occur mode (with consult) act more similarly
+  ;; to wgrep mode
+  (map! :map occur-mode-map
+        :n "C-x C-p" nil
+        :n "C-c C-p" #'occur-edit-mode)
+  (map! :map occur-edit-mode-map
+        :n "C-x C-q" nil
+        :n "C-c C-k" #'occur-cease-edit
+        :n "Z Z" #'occur-cease-edit))
