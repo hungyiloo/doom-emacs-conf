240f8a0f414a36912da32de9b96024a3d65c18b9
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2021-03-10 17:33:47 +0800

    Disable angular-ls for now. It's too heavy.

--- config/lsp-mode.el
+++ config/lsp-mode.el
@@ -6,9 +6,13 @@
   (setq lsp-enable-symbol-highlighting nil)
   (setq lsp-enable-file-watchers nil)
 
+  (setq lsp-clients-typescript-server-args
+        `("--stdio"
+          ,(concat "--tsserver-path=" (getenv "HOME") "/.yarn/bin/tsserver")))
+
   ;; NOTE: Angular/TS language servers might perform better if we have this
   ;; (setenv "TSC_NONPOLLING_WATCHER" "1")
-
+  (setq lsp-disabled-clients '(angular-ls)) ; disabled for performance for now. chews up so much CPU!
   (setq lsp-clients-angular-language-server-command
         `("node"
           ,(doom-path (getenv "HOME") ".config/yarn/global/node_modules/@angular/language-server")
@@ -17,9 +21,7 @@
           "--tsProbeLocations"
           ,(doom-path (getenv "HOME") ".config/yarn/global/node_modules")
           "--stdio"))
-  (setq lsp-clients-typescript-server-args
-        `("--stdio"
-          ,(concat "--tsserver-path=" (getenv "HOME") "/.yarn/bin/tsserver")))
+
   ;; (setq lsp-headerline-breadcrumb-enable t)
   ;; (setq lsp-headerline-breadcrumb-enable-diagnostics nil)
   (setq lsp-semantic-tokens-enable t))
