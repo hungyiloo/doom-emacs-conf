614539d7bd80efdf458f6d95148927f06bacfd79
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2021-03-12 12:30:40 +0800

    Set up quick keys consistently for all packages that use it

--- config/selectrum.el
+++ config/selectrum.el
@@ -1,4 +1,5 @@
 ;;; config/selectrum.el -*- lexical-binding: t; -*-
 
 (after! selectrum
-  (setq selectrum-max-window-height 12))
+  (setq selectrum-max-window-height 12)
+  (setq selectrum-quick-keys '(?a ?r ?s ?t ?n ?e ?i ?o ?d ?h)))
