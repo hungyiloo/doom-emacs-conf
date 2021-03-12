d737bc7bc3130bf22381b2545f73b9f23cdd7292
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2021-03-12 10:31:34 +0800

    Fix tree sitter config (wasn't starting up)

--- config/tree-sitter.el
+++ config/tree-sitter.el
@@ -1,8 +1,9 @@
 ;;; config/tree-sitter.el -*- lexical-binding: t; -*-
 
-(after! tree-sitter
+(use-package! tree-sitter
+  :hook (doom-first-buffer . global-tree-sitter-mode)
+  :config
   (require 'tree-sitter-langs)
-  (global-tree-sitter-mode)
   (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
   ;; Uncomment this section to use CSS tree-sitter highlighting for scss.
   ;; It works OK, but doesn't work for regular double slash comments.
