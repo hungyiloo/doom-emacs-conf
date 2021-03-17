84335c010819e98321b7ed089dff08c8c79d5d8f
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2021-03-17 16:45:21 +0800

    LSP config optimizations & enhancements (lsp-ui-peek)

--- config/lsp-mode.el
+++ config/lsp-mode.el
@@ -1,10 +1,20 @@
 ;;; config/lsp-mode.el -*- lexical-binding: t; -*-
 
 (after! lsp-mode
-  ;; Setting this disables DOOM's deferred shutdown functionality.
-  (setq +lsp-defer-shutdown nil)
-  (setq lsp-enable-symbol-highlighting nil)
-  (setq lsp-enable-file-watchers nil)
+  (setq +lsp-defer-shutdown nil
+        lsp-enable-symbol-highlighting nil
+        lsp-enable-file-watchers nil
+        lsp-log-io nil
+        lsp-enable-folding nil
+        lsp-semantic-tokens-enable nil
+        lsp-enable-links nil
+        lsp-enable-snippet t)
+
+  ;; Don't autocomplete snippets in LSP by default.
+  ;; It seems to interfere a lot more than it helps.
+  ;; Use the C-x C-s binding to call company-snippets manually.
+  (setq +lsp-company-backends '(:separate company-capf))
+
 
   (setq lsp-clients-typescript-server-args
         `("--stdio"
@@ -23,6 +33,27 @@
           ,(doom-path (getenv "HOME") ".config/yarn/global/node_modules")
           "--stdio"))
 
-  ;; (setq lsp-headerline-breadcrumb-enable t)
-  ;; (setq lsp-headerline-breadcrumb-enable-diagnostics nil)
-  (setq lsp-semantic-tokens-enable t))
+  ;; Load the lsp-command-map earlier, up front.
+  ;; In practice, it's never there early enough when I need it.
+  ;; e.g. without this, I keep getting "SPC c l s r is undefined"
+  ;; when entering keychords quickly.
+  ;; With this, it seems to be loaded along with lsp-mode
+  (map! :leader "c l" lsp-command-map)
+  (dolist (leader-key (list doom-leader-key doom-leader-alt-key))
+    (let ((lsp-keymap-prefix (concat leader-key " c l")))
+      (lsp-enable-which-key-integration))))
+
+(after! lsp-ui
+  ;; Fixes code action lightbulb icon background
+  ;; It's probably a compatiblity issue with solaire mode
+  (setq lsp-ui-sideline-actions-icon (doom-path doom-private-dir "assets" "lightbulb.png"))
+
+  (custom-set-faces!
+    `(lsp-ui-peek-highlight :foreground ,(doom-color 'yellow) :background ,(doom-color 'base4) :box t))
+
+  ;; This fixes the TAB file toggle in lsp-ui peeking.
+  ;; For some reason, evil's TAB keybinding takes precedence?
+  ;; This rebinding is a hack to work around that.
+  ;; Not sure of a better fix right now.
+  (map! :map lsp-ui-peek-mode-map
+        "<tab>" #'lsp-ui-peek--toggle-file))
