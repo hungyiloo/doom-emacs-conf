0c7e1debf11a0b190b1b6afc4343c6e927c83ea5
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2021-03-25 10:50:47 +0800

    Rework olivetti to be manually toggled & fix conflict with org-roam

--- config/olivetti.el
+++ config/olivetti.el
@@ -1,11 +1,21 @@
 ;;; config/olivetti.el -*- lexical-binding: t; -*-
 
 (use-package! olivetti
-  :hook (org-mode . olivetti-mode)
-  :hook (markdown-mode . olivetti-mode)
   :hook (olivetti-mode . my/olivetti-setup)
   :init
   (setq olivetti-body-width 80)
   (defun my/olivetti-setup ()
-    (setq doom--line-number-style nil)
-    (setq display-line-numbers nil)))
+    (setq doom--line-number-style (not olivetti-mode))
+    (setq display-line-numbers (not olivetti-mode))
+    (git-gutter-mode (if olivetti-mode -1 1)))
+  (map! :leader
+        (:prefix-map ("t" . "toggle")
+         "z" #'olivetti-mode))
+  :config
+  (after! org-roam
+    (advice-add #'org-roam-capture
+                :around
+                (defun my/disable-olivetti-for-roaming (orig-fun &rest args)
+                  (when olivetti-mode
+                    (olivetti-mode -1))
+                  (apply orig-fun args)))))
