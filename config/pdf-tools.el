353b98113654bc4a43a2650c863c1885852e5070
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2021-03-31 19:28:03 +0800

    Enable PDF tools

---
+++ config/pdf-tools.el
@@ -0,0 +1,6 @@
+;;; config/pdf-tools.el -*- lexical-binding: t; -*-
+
+(after! pdf-tools
+  (custom-set-faces!
+    `(pdf-isearch-match :foreground "#000" :background "#f7f")
+    `(pdf-isearch-lazy :foreground "#000" :background "#fd0")))
