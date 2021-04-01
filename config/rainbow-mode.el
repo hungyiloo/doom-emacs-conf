cde721a2659689e473330120688cd5ffdfe6993f
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2021-04-01 20:31:28 +0800

    Add toggle keybinding for rainbow-mode
    It's handy in a lot more places than I expect

--- config/rainbow-mode.el
+++ config/rainbow-mode.el
@@ -1,7 +1,13 @@
 ;;; config/rainbow-mode.el -*- lexical-binding: t; -*-
 
-(after! rainbow-mode
-  (setq rainbow-html-colors-major-mode-list '(html-mode css-mode scss-mode php-mode nxml-mode xml-mode))
+(use-package! rainbow-mode
+  :defer t
+  :init
+  (map! :leader
+        (:prefix-map ("t" . "toggle")
+         :desc "Rainbow" "c" #'rainbow-mode))
+  :config
+  (setq rainbow-html-colors-major-mode-list '(html-mode css-mode scss-mode js2-mode php-mode nxml-mode xml-mode))
   ;; Enhance rainbow-mode to use overlays instead of text properties.
   ;; This prevents inteferefence with hl-line-mode.
   ;; Source: https://github.com/amosbird/rainbow-mode
