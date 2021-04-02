31b478c07f89be6eda6b29f065be3da745c5869f
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2021-04-02 16:12:01 +0800

    Ensure CTRLF searching clears evil highlighting

--- config/ctrlf.el
+++ config/ctrlf.el
@@ -8,4 +8,19 @@
 
   ;; Disable ctrlf in pdf-tools isearch
   (add-hook! 'pdf-isearch-minor-mode-hook
-    (ctrlf-local-mode -1)))
+    (ctrlf-local-mode -1))
+
+  ;; Clear evil ex search highlights when starting ctrlf search
+  (defun my/ctrlf-clear-evil-ex-highlights-before (&rest _args)
+    (+evil-disable-ex-highlights-h))
+  (advice-add #'ctrlf-forward-literal :before #'my/ctrlf-clear-evil-ex-highlights-before)
+  (advice-add #'ctrlf-backward-literal :before #'my/ctrlf-clear-evil-ex-highlights-before)
+  (advice-add #'ctrlf-forward-regexp :before #'my/ctrlf-clear-evil-ex-highlights-before)
+  (advice-add #'ctrlf-backward-regexp :before #'my/ctrlf-clear-evil-ex-highlights-before)
+  (advice-add #'ctrlf-forward-symbol :before #'my/ctrlf-clear-evil-ex-highlights-before)
+  (advice-add #'ctrlf-forward-symbol-at-point :before #'my/ctrlf-clear-evil-ex-highlights-before)
+  (advice-add #'ctrlf-forward-fuzzy :before #'my/ctrlf-clear-evil-ex-highlights-before)
+  (advice-add #'ctrlf-forward-fuzzy-regexp :before #'my/ctrlf-clear-evil-ex-highlights-before)
+  (advice-add #'ctrlf-backward-fuzzy :before #'my/ctrlf-clear-evil-ex-highlights-before)
+  (advice-add #'ctrlf-backward-fuzzy-regexp :before #'my/ctrlf-clear-evil-ex-highlights-before)
+  (advice-add #'ctrlf-occur :before #'my/ctrlf-clear-evil-ex-highlights-before))
