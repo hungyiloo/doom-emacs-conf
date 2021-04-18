6fa6f1d14e8948a7785f46f6a74e4078e25e2c03
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2021-04-18 10:52:12 +0800

    Bump pinned packages

--- modules/completion/compres/packages.el
+++ modules/completion/compres/packages.el
@@ -3,28 +3,28 @@
 
 (package! embark
   :recipe (:host github :repo "oantolin/embark")
-  :pin "5f3097824f8c3d17bcd70c4e4ce597bcfcf2196f")
+  :pin "b8f82c9b6bdcd6e4d53e7d0bad869f3c8a8ef2f9")
 (package! embark-consult
   :recipe (:host github :repo "oantolin/embark")
-  :pin "5f3097824f8c3d17bcd70c4e4ce597bcfcf2196f")
+  :pin "b8f82c9b6bdcd6e4d53e7d0bad869f3c8a8ef2f9")
 (package! consult
   :recipe (:host github :repo "minad/consult")
-  :pin "846c715ee1df94292a9bb2467810bd7959ccf078")
+  :pin "923a34330207ed868b6388acf0f432ae989f1427")
 (package! consult-flycheck
   :recipe (:host github :repo "minad/consult")
-  :pin "846c715ee1df94292a9bb2467810bd7959ccf078")
+  :pin "923a34330207ed868b6388acf0f432ae989f1427")
 (package! marginalia
   :recipe (:host github :repo "minad/marginalia")
-  :pin "668265af921285c726b2239dae32459bd1064d03")
+  :pin "0c2735eea5e0803921903dc5f7294dcdeece47d2")
 (package! selectrum
   :recipe (:host github :repo "raxod502/selectrum")
-  :pin "9c5f142107d28868748a5801fb53ca7c5ad75fec")
+  :pin "d01718e9bbdf385205fb9c713caa64864f249392")
 (package! prescient
   :recipe (:host github :repo "raxod502/prescient.el")
-  :pin "bf0ddeb0b687e6af50ad82558bd32c17a2c0311b")
+  :pin "ed2b762241bbea03e374dc9dcd4fbe207c6b2ea4")
 (package! selectrum-prescient
   :recipe (:host github :repo "raxod502/prescient.el")
-  :pin "bf0ddeb0b687e6af50ad82558bd32c17a2c0311b")
+  :pin "ed2b762241bbea03e374dc9dcd4fbe207c6b2ea4")
 (package! orderless
   :recipe (:host github :repo "oantolin/orderless")
   :pin "87ab7e47e343285f7afd42779c78551332b8fd84")
