acabedd702f7e3222e7f4a29259e7f738b37e83b
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2021-04-18 10:51:37 +0800

    Put restclient-mode results in popup

--- config/restclient-mode.el
+++ config/restclient-mode.el
@@ -1,8 +1,9 @@
 ;;; config/restclient-mode.el -*- lexical-binding: t; -*-
 
-(set-popup-rule!
+(after! restclient-mode
+  (set-popup-rule!
     (format "\\(%s\\|%s\\)"
             restclient-same-buffer-response-name
             restclient-info-buffer-name)
-    :height 30
-    :quit t)
+    :height 0.6
+    :quit t))
