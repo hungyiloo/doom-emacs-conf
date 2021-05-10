b2a49368b0d97b35838d04a8db860407e9fd16d9
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2021-05-10 19:45:24 +0800

    Add WSL useful scripts to repo

---
+++ scripts/runemacs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source ~/.profile
+export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
+setxkbmap "us(colemak)"
+if ! pgrep -f syncthing &> /dev/null 2>&1; then
+	/usr/bin/syncthing -no-browser -logfile=default &>/dev/null &
+	disown
+fi
+cd ~
+~/.config/emacs/bin/doom env
+# setsid emacsclient -c -a=""
+~/.local/bin/emacs
