4836be390d469a019cbe978705c57c3e4d611d79
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-05-12 22:31:51 +0800

    Fix tsx comment line return error and personalize indent level to 2

--- config/tsx-mode.el
+++ config/tsx-mode.el
@@ -59,6 +59,8 @@
              (rainbow-delimiters-mode 1)
              (setq-local rainbow-delimiters-pick-face-function #'tsx-rainbow-delimiters-pick-face)
              (emmet-mode 1)
+             (require 'js2-mode)
+             (setq-local typescript-indent-level 2)
              (lsp!))
 
            ;; (advice-add #'js-syntax-propertize :around #'ignore)
