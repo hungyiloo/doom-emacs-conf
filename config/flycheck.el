8e8f0ebc188e2a3e8320076ce58003012f0874b4
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-05-22 18:57:32 +0800

    Prevent "flycheck is disabled" warnings (I don't care)

--- config/flycheck.el
+++ config/flycheck.el
@@ -3,4 +3,10 @@
 (after! flycheck
   (map! :leader
         (:prefix-map ("c" . "code")
-         "x" flycheck-command-map)))
+         "x" flycheck-command-map))
+
+  (advice-add #'flycheck-buffer
+              :around
+              (defun my/flycheck-ignore-when-disabled-advice (orig-fun &rest args)
+                (when flycheck-mode
+                    (apply orig-fun args)))))
