6f4a0a97c71cd0fa1687504b252cd0895f4910c9
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-07-12 21:27:33 +0800

    Bump pinned packages

--- modules/completion/compres/packages.el
+++ modules/completion/compres/packages.el
@@ -9,16 +9,16 @@
   :pin "acbe1cba548832d295449da348719f69b9685c6f")
 (package! consult
   :recipe (:host github :repo "minad/consult")
-  :pin "6ce9aa58e74da92e391cfe938bc3da4f47ab591e")
+  :pin "f17db9520ddd612dc837f4112b6bcbb172acef85")
 (package! consult-flycheck
   :recipe (:host github :repo "minad/consult")
-  :pin "6ce9aa58e74da92e391cfe938bc3da4f47ab591e")
+  :pin "f17db9520ddd612dc837f4112b6bcbb172acef85")
 (package! marginalia
   :recipe (:host github :repo "minad/marginalia")
   :pin "3bf0a4db55f6267467f0a08715f4776509a3b503")
 (package! selectrum
   :recipe (:host github :repo "raxod502/selectrum")
-  :pin "a19bbe94de492bf504399c093cfc5695eb630fa8")
+  :pin "48ea51aa5b6959ea2a134e36cd21f727047b0677")
 (package! prescient
   :recipe (:host github :repo "raxod502/prescient.el")
   :pin "4a0f5405798cfcb98ea005078ef2e2d490e922c4")
