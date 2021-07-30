66e38c1728116e9d42335ac37e79f6d562634ffb
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-07-30 20:24:23 +0800

    Make vertico scroll page up and down like I'm used to

--- config/vertico.el
+++ config/vertico.el
@@ -3,8 +3,8 @@
 (after! vertico
   (setq vertico-cycle nil)
   (map! :map vertico-map
-       "C-d" (cmd! (vertico-next vertico-count))
-       "C-u" (cmd! (vertico-previous vertico-count))
+       "C-d" #'vertico-scroll-up ;; these are reversed for some reason?!
+       "C-u" #'vertico-scroll-down
        "C-k" #'kill-line))
 
 (use-package! vertico-mouse
