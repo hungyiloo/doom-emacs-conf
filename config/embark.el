9757b4083652f2a1385cf2342f32dd0def8d1946
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-08-01 17:48:28 +0800

    Fix embark-prefix-help-command binding in which-key
    prefix-help-command was being reset by helpful package. Not sure how or why.

--- config/embark.el
+++ config/embark.el
@@ -1,5 +1,10 @@
 ;;; config/embark.el -*- lexical-binding: t; -*-
 
 (use-package! embark
+  :defer t
   :init
+  (setq prefix-help-command #'embark-prefix-help-command))
+
+;; Helpful tries to reset `prefix-help-command', so we set it back
+(after! helpful
   (setq prefix-help-command #'embark-prefix-help-command))
---
+++ config/which-key.el
@@ -0,0 +1,4 @@
+;;; config/which-key.el -*- lexical-binding: t; -*-
+
+(setq which-key-use-C-h-commands nil)
+
