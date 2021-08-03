9c028485d8efb1fc14df34305a11127ba8b7c3d7
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-08-03 19:06:47 +0800

    Smoother embark-cycling when calling from C-;

--- config/embark.el
+++ config/embark.el
@@ -2,8 +2,15 @@
 
 (use-package! embark
   :defer t
+
   :init
-  (setq prefix-help-command #'embark-prefix-help-command))
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  :config
+  ;; Map the alternative embark-act keybinding to cycle when embark already
+  ;; activated, just like the SPC a binding
+  (map! :map embark-general-map
+        "C-;" #'embark-cycle))
 
 ;; Helpful tries to reset `prefix-help-command', so we set it back
 (after! helpful
