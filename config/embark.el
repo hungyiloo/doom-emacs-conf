ccc57a2921e9d8a7b91fa8d55a75fa55e33da42d
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-08-15 16:17:51 +0800

    Use doom's built-in support for embark-prefix-help-command

--- config/embark.el
+++ config/embark.el
@@ -3,15 +3,8 @@
 (use-package! embark
   :defer t
 
-  :init
-  (setq prefix-help-command #'embark-prefix-help-command)
-
   :config
   ;; Map the alternative embark-act keybinding to cycle when embark already
   ;; activated, just like the SPC a binding
   (map! :map embark-general-map
         "C-;" #'embark-cycle))
-
-;; Helpful tries to reset `prefix-help-command', so we set it back
-(after! helpful
-  (setq prefix-help-command #'embark-prefix-help-command))
