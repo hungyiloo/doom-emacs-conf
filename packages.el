d15450cf2a3298ea3cb79e695dd4b63ae2f73484
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-08-15 16:18:56 +0800

    Bump pinned packages

--- packages.el
+++ packages.el
@@ -36,19 +36,22 @@
 ;; rainbow-mode without the extra rgb module stuff (e.g. kurecolor)
 (package! rainbow-mode)
 
-(package! lsp-mode :pin "5b2daf688e7bf2590b1d1c8cea06fa57114282f9")
+(package! lsp-mode :pin "32d34445f3c4da37840c0ebb3a12e2c0627782a8")
 (package! lsp-ui :pin "177c31e982345ba35dc7c5d90cb1f8e68585323a")
 ;; (package! org-roam :pin "756f6215b672e267f986a3d6e494f5309825b91a")
 (package! ctrlf :pin "b78e129a8a4fabfebba8cdd5ef51278d0d57e0f4")
-(package! org-mode :pin "8df3e25db8a629bbffa883edadb13a55239ae4b7")
+(package! org-mode :pin "09cf4a075efe62262cb103605c5bd9c6f5cdcae2")
 (package! vterm :pin "f73644a07ce56712101d1568139a8f3c3b4a9e59")
 ;; (package! good-scroll :pin "fb01f121c4c77db3e6750303894d57b31e410b14")
 ;; (package! evil :pin "f5ab7ff")
 ;; (package! evil-mc :pin "f04fb17")
 ;; (package! doom-themes :pin "55f01ed")
+(package! embark
+  :recipe (:host github :repo "oantolin/embark")
+  :pin "fcd70f55918e0f015418cb00c20b04d282cb211b")
 (package! vertico-mouse
   :recipe (:host github :repo "minad/vertico")
-  :pin "5e612540ed380394fda38cc7938b8734c7001bca")
+  :pin "d00ad0697024aa9555972ea2bce678080272d633")
 
 (package! tsx-mode
   :recipe (:local-repo "lisp"))
