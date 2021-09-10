90a8431c00391b21ecf8c7523c8ae113b77569ca
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-09-10 16:41:11 +0800

    Remove obsoleted spelling configuration

--- config/ispell.el
+++ config/ispell.el
@@ -1,10 +1,4 @@
 ;;; config/ispell.el -*- lexical-binding: t; -*-
 
-(after! ispell
-  (setq ispell-dictionary "en"))
-
-(after! spell-fu
-  (add-to-list 'spell-fu-faces-include 'tree-sitter-hl-face:comment)
-  (add-to-list 'spell-fu-faces-include 'tree-sitter-hl-face:doc)
-  (add-to-list 'spell-fu-faces-include 'tree-sitter-hl-face:string)
-  (add-to-list 'spell-fu-faces-exclude 'tree-sitter-hl-face:embedded))
+;; (after! ispell
+;;   (setq ispell-dictionary "en"))
