c66f0acc5b0bb548a32d7c0249ff73358f35e098
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-09-28 21:28:17 +0800

    ediff support "choose both"

--- config/ediff.el
+++ config/ediff.el
@@ -3,4 +3,13 @@
 (after! ediff
   ;; Include ediff buffers in solaire-mode so they look the same
   ;; as regular editing buffers
-  (add-hook! 'ediff-prepare-buffer-hook #'solaire-mode))
+  (add-hook! 'ediff-prepare-buffer-hook #'solaire-mode)
+
+  (defun ediff-copy-both-to-C ()
+    (interactive)
+    (ediff-copy-diff ediff-current-difference nil 'C nil
+                     (concat
+                      (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                      (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+  (defun add-&-to-ediff-mode-map () (define-key ediff-mode-map "&" 'ediff-copy-both-to-C))
+  (add-hook 'ediff-keymap-setup-hook #'add-&-to-ediff-mode-map))
