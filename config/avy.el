d6df31b69afa27a7c61c59627b37e7b09a9e2287
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-11-27 15:26:54 +0800

    Add embark to avy and set up better dispatching keys

--- config/avy.el
+++ config/avy.el
@@ -1,5 +1,25 @@
-;;; config/avy.el -*- lexical-binding: t; -*-
+;;a config/avy.el -*- lexical-binding: t; -*-
 
 (after! avy
   ;; Configure avy to use colemak home row
-  (setq avy-keys '(?a ?r ?s ?t ?n ?e ?i ?o ?d ?h)))
+  (setq avy-keys '(?a ?r ?s ?t ?n ?e ?i ?o ?d ?h))
+
+  (defun avy-action-embark (pt)
+    (unwind-protect
+        (save-excursion
+          (goto-char pt)
+          (embark-act))
+      (select-window
+       (cdr (ring-ref avy-ring 0))))
+    t)
+
+  (setq avy-dispatch-alist '((59 . avy-action-embark)
+                             (?k . avy-action-kill-move)
+                             (?K . avy-action-kill-stay)
+                             (?< . avy-action-teleport)
+                             (?m . avy-action-mark)
+                             (?w . avy-action-copy)
+                             (?y . avy-action-yank)
+                             (?Y . avy-action-yank-line)
+                             (?= . avy-action-ispell)
+                             (?z . avy-action-zap-to-char))))
