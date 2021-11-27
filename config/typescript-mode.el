a8dc691ff3902dccd5d13508857c4ae91bb75126
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-11-27 15:25:15 +0800

    Don't use tsx-mode for typescript because of <typing>

--- config/typescript-mode.el
+++ config/typescript-mode.el
@@ -3,4 +3,5 @@
 (after! typescript-mode
   (setq typescript-indent-level 2)
   (setq tide-native-json-parsing t)
-  (setq tide-completion-ignore-case t))
+  (setq tide-completion-ignore-case t)
+  (require 'js2-mode))
