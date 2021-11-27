eb90231c4f6885750e17c9c349885fac530a2ad0
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-11-27 15:26:02 +0800

    Add titlecasing to embark

--- config/embark.el
+++ config/embark.el
@@ -7,4 +7,12 @@
   ;; Map the alternative embark-act keybinding to cycle when embark already
   ;; activated, just like the SPC a binding
   (map! :map embark-general-map
-        "C-;" #'embark-cycle))
+        "C-;" #'embark-cycle)
+
+  (map! :map embark-prose-map
+        "`" #'titlecase-region)
+
+  (map! :map embark-region-map
+        "`" #'titlecase-region)
+
+  (add-to-list 'embark-pre-action-hooks '(titlecase-region embark--mark-target)))
