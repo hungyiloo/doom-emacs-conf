006f47f1435853127fd3bf79182bf250b3536e4c
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-12-20 21:14:42 +0800

    Remove unnecessary use-package!

--- config/titular.el
+++ config/titular.el
@@ -1,6 +1,4 @@
 ;;; config/titular.el -*- lexical-binding: t; -*-
-(use-package! titular
-  :commands (titlecase-region titlecase-dwim))
 
 (after! evil
   (map! :nv "g`" #'my/evil-titlecase-operator))
