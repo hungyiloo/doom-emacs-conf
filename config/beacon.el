e2340d941ac42aeee4b569d7e125a326be16baf8
Hung-Yi Loo <hungyi.loo@gmail.com>
2022-01-20 19:32:59 +0800

    less beacon blinking

--- config/beacon.el
+++ config/beacon.el
@@ -5,12 +5,8 @@
   (beacon-mode +1)
 
   :config
-  (setq beacon-blink-when-focused t)
-  (setq beacon-blink-when-point-moves-vertically 10)
+  (setq beacon-blink-when-focused t
+        beacon-blink-when-point-moves-vertically 10)
 
-  (after! evil
-    (defun my/beacon-blink-advice (&rest ignored) (beacon-blink))
-    (advice-add #'evil-scroll-up :after #'my/beacon-blink-advice)
-    (advice-add #'evil-scroll-down :after #'my/beacon-blink-advice)
-    (advice-add #'evil-scroll-page-up :after #'my/beacon-blink-advice)
-    (advice-add #'evil-scroll-page-down :after #'my/beacon-blink-advice)))
+  (add-to-list 'beacon-dont-blink-major-modes 'vterm-mode)
+  (add-to-list 'beacon-dont-blink-major-modes '+doom-dashboard-mode))
