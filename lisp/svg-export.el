da497b54c8be91e0caa7b0dc70ec2e11b3f9a880
Hung-Yi Loo <hungyi.loo@gmail.com>
2022-07-10 18:45:38 +0800

    Add a function to take a SVG screenshot of emacs

---
+++ lisp/svg-export.el
@@ -0,0 +1,12 @@
+;;; lisp/svg-export.el -*- lexical-binding: t; -*-
+
+(defun screenshot-svg ()
+  "Save a screenshot of the current frame as an SVG image.
+Saves to a temp file and puts the filename in the kill ring."
+  (interactive)
+  (let* ((filename (make-temp-file "Emacs" nil ".svg"))
+         (data (x-export-frames nil 'svg)))
+    (with-temp-file filename
+      (insert data))
+    (kill-new filename)
+    (message filename)))
