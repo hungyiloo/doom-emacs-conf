302be5f2283a0b5abe515302104176545a8e616c
Hung-Yi Loo <hungyi.loo@gmail.com>
2023-03-11 19:50:28 +0800

    Update fix-weston hack to the version I've been using

--- scripts/fix-weston.sh
+++ scripts/fix-weston.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
-if [ ! -f /mnt/wsl/state-wslg-config-initialized ]; then
-echo DISPLAY=$DISPLAY
-echo Patching weston...
-sleep 3
-KEYMAP_LAYOUT=us
-KEYMAP_VARIANT=colemak
-read -r -d '' GWSL_SYSTEM_CONFIG_COMMANDS <<EOF
+#!/usr/bin/env bash
+if [ ! -f /mnt/wsl/state-wslg-config-initialized ];
+then
+    echo Patching weston config.ini with keyboard layout hack...
+    KEYMAP_LAYOUT=us
+    KEYMAP_VARIANT=colemak
+    read -r -d '' GWSL_SYSTEM_CONFIG_COMMANDS <<EOF
 KEYMAP_LAYOUT=\`grep -r '^keymap_layout=.*\$' /home/wslg/.config/weston.ini\`;if [ -z \$KEYMAP_LAYOUT ]; then sed -i '$ a\[keyboard]\nkeymap_layout=`echo ${KEYMAP_LAYOUT}`\nkeymap_variant=`echo ${KEYMAP_VARIANT}`\n' /home/wslg/.config/weston.ini;pkill -HUP weston;touch /mnt/wsl/state-wslg-config-initialized;fi
 EOF
-wsl.exe -d $WSL_DISTRO_NAME --system ${GWSL_SYSTEM_CONFIG_COMMANDS}
+    wsl.exe -d $WSL_DISTRO_NAME --system ${GWSL_SYSTEM_CONFIG_COMMANDS}
+else
+    echo Already patched weston config.ini with keyboard layout hack in this session.
 fi
