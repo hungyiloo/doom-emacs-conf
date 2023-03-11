031414e6949be264e5c87f72824fe07cfd0d830f
Hung-Yi Loo <hungyi.loo@gmail.com>
2023-03-11 19:51:47 +0800

    Add some convenience key mappings in ledger mode

--- config/ledger.el
+++ config/ledger.el
@@ -2,4 +2,8 @@
 
 (after! ledger-mode
   (add-hook! 'ledger-mode-hook
-    (spell-fu-mode -1)))
+    (spell-fu-mode -1))
+  (map! :map ledger-mode-map
+        :nv "[p" #'ledger-navigate-previous-uncleared
+        :nv "]p" #'ledger-navigate-next-uncleared
+        :nv "SPC m SPC" #'ledger-toggle-current))
