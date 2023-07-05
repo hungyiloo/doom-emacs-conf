f6d5dec0d8c42856ec1900acfb8f27873ce8bbe4
Hung-Yi Loo <hungyi.loo@gmail.com>
2023-07-05 19:04:12 +0800

    Fix case sensitivity reserved word issues with literate-calc var names

--- config/calc.el
+++ config/calc.el
@@ -2,3 +2,13 @@
 
 (after! calc
   (defalias 'calcFunc-uconv 'math-convert-units))
+
+(after! literate-calc-mode
+  ;; Fix case sensitive reserved word ignoring in literate calc variable
+  ;; bindings that interfere with variable names like "Working Day" and
+  ;; "Internet Usage"
+  (advice-add #'literate-calc--substitute-variable-values
+              :around
+              (defun my/literate-calc--subsitute-variable-values (orig-fun &rest args)
+                (let ((case-fold-search nil))
+                  (apply orig-fun args)))))
